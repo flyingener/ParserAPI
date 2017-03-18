c8a181bc19f345f85063949c742fc05f886b55cf
flyingener <1991y10m15d@gmail.com>
2017-03-19 00:11:11 +0300

    Add
    Add project in Github

---
+++ ParserAPI.prj/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ParserAPI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.btnChart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Controls.Add(this.btnRequest);
+            this.panel1.Controls.Add(this.btnChart);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(629, 418);
+            this.panel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(320, 4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(297, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(380, 392);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnRequest.TabIndex = 3;
+            this.btnRequest.Text = "Запрос";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            this.btnRequest.Click += new System.EventHandler(this.btnRequest_Click);
+            // 
+            // btnChart
+            // 
+            this.btnChart.Location = new System.Drawing.Point(461, 392);
+            this.btnChart.Name = "btnChart";
+            this.btnChart.Size = new System.Drawing.Size(75, 23);
+            this.btnChart.TabIndex = 2;
+            this.btnChart.Text = "Вывод";
+            this.btnChart.UseVisualStyleBackColor = true;
+            this.btnChart.Click += new System.EventHandler(this.btnChart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(542, 392);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(310, 300);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 418);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "ParserAPI";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.Button btnChart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ParserAPI.prj/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ParserAPI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public string text;
+
+        public int Offset;
+
+        public DateTime Week = DateTime.Now.AddDays(-7);
+
+        //List<ParserResultData> ParserResult = new List<ParserResultData>();
+
+
+        public class ParserResult
+        {
+            public int id { get; set; }
+            public string plate { get; set; }
+            public DateTime timestamp { get; set; }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            Offset = 0;
+
+            int done = 0;
+
+            //Запускаем цикл до того момента, пока не будут получены более ранние записи
+
+            while (done <= 1)
+            {
+                //Создание web-запроса
+
+                HttpWebRequest myWebRequest = (HttpWebRequest)WebRequest.Create("http://xn--80aaahbralm5bfdcfjcdqpf.xn--p1ai:45555/api/v1/vehicles?offset=" + Offset + "&limit=15");
+
+                myWebRequest.Accept = "application/json";
+
+                myWebRequest.Method = "GET";
+
+                //Отправка запроса 
+
+                HttpWebResponse myWebResponse = (HttpWebResponse)myWebRequest.GetResponse();
+
+                var serializer = JsonSerializer.CreateDefault();
+
+                var listEnteringAuto = new ParserResult();
+
+                //Обработка ответа
+
+                using (StreamReader stream = new StreamReader(myWebResponse.GetResponseStream(), Encoding.UTF8))
+                {
+                    using (var jsonReader = new JsonTextReader(stream))
+                    {
+                        //Подготовка к парсингу json ответа
+                        JObject jsonParser = JObject.Parse(stream.ReadToEnd());
+
+                        List<JToken> results = jsonParser["entries"].Children().ToList();
+
+                        List<ParserResult> ParserResult = new List<ParserResult>();
+
+                        foreach (JToken result in results)
+                        {
+                            //Десериализация ответа и передача полученных значений
+
+                            ParserResult searchResult = JsonConvert.DeserializeObject<ParserResult>(result.ToString());
+
+                            ParserResult.Add(searchResult);
+                        }
+
+                        //Проверка даты, среди полученных данных
+
+                        foreach (ParserResult result in ParserResult)
+                        {
+                            //Тестирование результата и передача его в richTextBox, для анализа
+
+                            text = text + "ID: " + result.id + "; Timestamp: "
+                                 + result.timestamp + "; Number: " + result.plate;
+
+                            richTextBox1.Text = text;
+
+                            //Если получаем дату, которая была раньше, чем 7 дней назад, то возвращаем done = 1
+
+                            if (Week.CompareTo(result.timestamp) == 1)
+                            {
+                                done++;
+                            }
+                        }
+                    }
+                }
+
+                //Увеличиваем значение Offset, что бы пропустить записи, которые уже есть
+
+                Offset = Offset + 15;
+            }
+        }
+
+        private void btnChart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
